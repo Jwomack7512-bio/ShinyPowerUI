425e1ee27d0afba40c8d0b709329dca3bec0efc7
Jwomack7512-bio <jwomack@mcw.edu>
2023-09-06 23:36:20 -0400

    added plotResizable, minor tableLayout edits

---
+++ R/plotResizable.R
@@ -0,0 +1,111 @@
+#' Resizable Shiny Plot Output
+#'
+#' Wraps a Shiny plot output in a resizable container.
+#'
+#' @param plot_output A Shiny plot output element, typically from `plotOutput()`.
+#' @param autoResizeWithWindow Logical. If `TRUE`, the plot resizes automatically with the window. Default is `TRUE`.
+#' @param showBorderOnResize Logical. If `TRUE`, shows a dotted border around the plot while resizing. Default is `TRUE`.
+#'
+#' @return A Shiny UI element (HTML tags) that wraps the provided plot output, making it resizable.
+#'
+#' @examples
+#' \dontrun{
+#' library(shiny)
+#'
+#' ui <- fluidPage(
+#'   titlePanel("Resizable Plot Demo"),
+#'   sidebarLayout(
+#'     sidebarPanel(
+#'       helpText("Resize the plot by dragging from its edges.")
+#'     ),
+#'     mainPanel(
+#'       plotResizable(plotOutput("distPlot"))
+#'     )
+#'   )
+#' )
+#'
+#' server <- function(input, output) {
+#'   output$distPlot <- renderPlot({
+#'     hist(rnorm(100))
+#'   })
+#' }
+#'
+#' shinyApp(ui = ui, server = server)
+#' }
+#'
+#' @importFrom shiny tags
+#' @importFrom shiny HTML
+#' @export
+plotResizable <- function(plot_output,
+                          autoResizeWithWindow = TRUE,
+                          showBorderOnResize = TRUE) {
+
+  resizeScript <- if (autoResizeWithWindow) {
+    shiny::tags$script(HTML("
+      var prevWindowWidth = $(window).width();
+      var prevWindowHeight = $(window).height();
+
+      function setResizableLimits() {
+          var currentWindowWidth = $(window).width();
+          var currentWindowHeight = $(window).height();
+
+          var widthRatio = currentWindowWidth / prevWindowWidth;
+          var heightRatio = currentWindowHeight / prevWindowHeight;
+
+          var newPlotWidth = $('#resizablePlot').width() * widthRatio;
+          var newPlotHeight = $('#resizablePlot').height() * heightRatio;
+
+          $('#resizablePlot').width(newPlotWidth);
+          $('#resizablePlot .shiny-plot-output').width(newPlotWidth);
+          $('#resizablePlot').height(newPlotHeight);
+          $('#resizablePlot .shiny-plot-output').height(newPlotHeight);
+
+          prevWindowWidth = currentWindowWidth;
+          prevWindowHeight = currentWindowHeight;
+
+          Shiny.setInputValue('window_resized', Math.random());
+      }
+
+      $(window).resize(setResizableLimits);
+    "))
+  } else {
+    NULL
+  }
+
+  borderColor <- if (showBorderOnResize) "'#888'" else "'transparent'"
+
+  shiny::tags$div(
+    id = 'resizablePlot',
+    plot_output,
+
+    shiny::tags$head(
+      shiny::tags$link(rel = "stylesheet", href = "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"),
+      shiny::tags$script(src = "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"),
+      shiny::tags$style(HTML("
+        #resizablePlot {
+          border: 2px dashed transparent;  /* Initially transparent border */
+        }
+      ")),
+      shiny::tags$script(HTML(sprintf("
+        $(document).ready(function(){
+            $('#resizablePlot').resizable({
+                alsoResize: '#resizablePlot .shiny-plot-output',
+                start: function(event, ui) {
+                    $(this).css({
+                        'border-color': %s,  /* Conditionally set the border color when resizing starts */
+                        'z-index': '1000'
+                    });
+                },
+                stop: function(event, ui) {
+                    $(this).css({
+                        'border-color': 'transparent',  /* Reset the border color when resizing stops */
+                        'z-index': 'auto'
+                    });
+                }
+            });
+        });
+      ", borderColor))),
+      resizeScript
+    )
+  )
+}
--- R/tableLayout.R
+++ R/tableLayout.R
@@ -15,58 +15,87 @@
 #' if (interactive()) {
 #'   library(shiny)
 #'
-#'   ui <- fluidPage(
-#'     tableLayout(
-#'       textInput(inputId = "text1", label = "First Input", value = "Example"),
-#'       checkboxInput(inputId = "check1", label = "A Checkbox", value = TRUE)
+#'
+#' ui <- fluidPage(
+#'   titlePanel("Table Layout Example"),
+#'
+#'   sidebarLayout(
+#'     sidebarPanel(
+#'       selectInput("fontSize", "Font Size:", choices = c("14px", "16px", "18px")),
+#'       selectInput("paddingBottom", "Padding Bottom:", choices = c("5px", "10px", "15px")),
+#'       checkboxInput("isBold", "Bold Labels", value = TRUE)
+#'     ),
+#'
+#'     mainPanel(
+#'       uiOutput("tableOutput")
 #'     )
 #'   )
+#' )
 #'
-#'   server <- function(input, output, session) {}
+#' server <- function(input, output) {
 #'
-#'   shinyApp(ui, server)
+#'   output$tableOutput <- renderUI({
+#'     tableLayout(
+#'
+#'       textInput(inputId = "PI_CC_reactants", label = "Reactants", value = "", placeholder = "x1, x2"),
+#'       selectInput(inputId = "PI_CC_products",
+#'                   label = "Products",
+#'                   choices = c("a", "b", "c")),
+#'       checkboxInput(inputId = "PI_CC_modifiers",
+#'                     label = "Modifiers",
+#'                     value = TRUE),
+#'       numericInput("test1", "numebers", value = 3),
+#'       fontSize = input$fontSize,
+#'       paddingBottom = input$paddingBottom,
+#'       isBold = input$isBold
+#'     )
+#'   })
+#'
+#' }
+#'
+#' shinyApp(ui = ui, server = server)
 #' }
 #'
 
-tableLayout <- function(..., 
-                        firstColWidth = NULL, 
-                        fontSize = "16px", 
-                        paddingBottom = "15px", 
+tableLayout <- function(...,
+                        firstColWidth = NULL,
+                        fontSize = "16px",
+                        paddingBottom = "15px",
                         isBold = TRUE) {
-  
+
   widgets <- list(...)
-  
+
   labels <- character(length(widgets))
-  
+
   for (i in seq_along(widgets)) {
     current_widget <- widgets[[i]]
-    
+
     # Check for checkboxInput based on the structure
     if (is.list(current_widget$children[[1]]) &&
         is.list(current_widget$children[[1]]$children[[1]]) &&
         !is.null(current_widget$children[[1]]$children[[1]]$children[[2]]) &&
         is.list(current_widget$children[[1]]$children[[1]]$children[[2]])) {
-      
+
       labels[i] <- current_widget$children[[1]]$children[[1]]$children[[2]]$children
       widgets[[i]]$children[[1]]$children[[1]]$children[[2]] <- NULL
-      
+
       # Check for textInput and similar structures
-    } else if (is.list(current_widget$children[[1]]) && 
+    } else if (is.list(current_widget$children[[1]]) &&
                !is.null(current_widget$children[[1]]$children)) {
-      
+
       labels[i] <- current_widget$children[[1]]$children
       widgets[[i]]$children[[1]] <- NULL
     }
   }
-  
+
   fontWeight <- ifelse(isBold, "bold", "normal")
-  
+
   rows <- lapply(1:length(labels), function(i) {
     tags$tr(
       tags$td(
         div(
-          style = paste0("font-size:", fontSize, ";", 
-                         "padding-bottom:", paddingBottom, ";", 
+          style = paste0("font-size:", fontSize, ";",
+                         "padding-bottom:", paddingBottom, ";",
                          "font-weight:", fontWeight, ";"),
           labels[i]
         )
@@ -76,7 +105,7 @@ tableLayout <- function(...,
       )
     )
   })
-  
+
   # Adaptive CSS
   adaptive_css <- "
   .adaptive-table td:first-child {
@@ -84,26 +113,26 @@ tableLayout <- function(...,
     white-space: nowrap;
     padding-right: 10px;
   }
-  
+
   .adaptive-table td:last-child {
     width: 100%;
   }
   "
-  
+
   # If firstColWidth is set, adjust CSS accordingly
   fixed_width_css <- paste0(
     ".fixed-width-table td:first-child {
       width: ", firstColWidth, ";
     }
-    
+
     .fixed-width-table td:last-child {
       width: ", 100 - as.numeric(substr(firstColWidth, 1, nchar(firstColWidth) - 1)), "%;
     }"
   )
-  
+
   css_to_use <- ifelse(is.null(firstColWidth), adaptive_css, fixed_width_css)
   table_class <- ifelse(is.null(firstColWidth), "adaptive-table", "fixed-width-table")
-  
+
   shiny::tagList(
     shiny::tags$style(css_to_use),
     tags$table(class = paste("table-widgets", table_class), do.call(tagList, rows))
