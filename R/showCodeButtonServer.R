17cec254a108bb7248ce7971e09489666ac6c220
Jwomack7512-bio <jwomack@mcw.edu>
2024-01-01 21:35:39 -0500

    updated showCode Module

--- R/showCodeButtonServer.R
+++ R/showCodeButtonServer.R
@@ -1,8 +1,23 @@
-# Module server function
+#' Server function to create a show/hide code button
+#'
+#' This function sets up the server-side logic for a Shiny app to display a
+#' button that toggles the visibility of generated code.
+#'
+#' @param input Shiny input object.
+#' @param output Shiny output object.
+#' @param session Shiny session object.
+#' @param generated_code A function that returns the generated code.
+#'
+#' @return None
+#'
+#' @export
 showCodeButtonServer <- function(input,
                                  output,
                                  session,
                                  generated_code) {
+
+  # Variable to track the button state
+  button_state <- reactiveVal(FALSE)
 
   # Render show/hide code button and code text
   output$code_section <- shiny::renderUI({
@@ -12,13 +27,21 @@ showCodeButtonServer <- function(input,
   # Toggle code visibility on button click
   shiny::observeEvent(input$show_code_button, {
     shinyjs::toggle("code_div")
+    button_state(!button_state())
   })
 
   # Render the generated code text
   output$generated_code_text <- shiny::renderText({
     generated_code()
   })
+
+  # Update button label based on the state
+  observe({
+    label <- if (button_state()) "Hide Code" else "Show Code"
+    shiny::updateActionButton(session, "show_code_button", label = label)
+  })
 }
+
 
 
 # ui <- fluidPage(
--- R/showCodeButtonUI.R
+++ R/showCodeButtonUI.R
@@ -1,17 +1,36 @@
-showCodeButtonUI <- function(id) {
+#' UI function to create a show/hide code button
+#'
+#' This function generates the UI components for displaying a button that
+#' toggles the visibility of generated code.
+#'
+#' @param id A unique identifier for the UI elements.
+#'
+#' @param hyperlink_color Color of the hyperlink text (default: "blue").
+#'
+#' @param background_color Background color of the button (default: "transparent").
+#'
+#' @param border_style CSS style for the button border (default: "none").
+#'
+#' @return A Shiny UI element.
+#'
+#' @export
+showCodeButtonUI <- function(id,
+                             hyperlink_color = "blue",
+                             background_color = "transparent",
+                             border_style = "none") {
   ns <- shiny::NS(id)
   shiny::fluidRow(
     shiny::column(
       width = 12,
       # Button to show/hide code
       shiny::actionButton(ns("show_code_button"),
-                          label = shiny::HTML("</> Show Code"),
-                          icon = shiny::icon("code")),
+                          label = shiny::HTML(paste0("</> Show Code")),
+                          icon = shiny::icon("code"),
+                          style = sprintf("color: %s; background-color: %s; border: %s;", hyperlink_color, background_color, border_style)),
       # Display the generated code with toggle visibility
       shiny::div(
         id = ns("code_div"),
         style = "display:none;",
-        "TEST",
         shiny::verbatimTextOutput(ns("generated_code_text"))
       )
     )
