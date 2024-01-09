bb7ea5e70e556c9e12b7e73405f96af0c05591b9
Jwomack7512-bio <jwomack@mcw.edu>
2024-01-08 20:23:29 -0600

    added height option to titlePanel

--- R/titlePanel.R
+++ R/titlePanel.R
@@ -3,8 +3,9 @@
 #' The `titlePanel` function provides a way to create a styled panel
 #' in Shiny applications. This custom panel can include widgets,
 #' an optional title, and several style customizations.
+#'
 #' @importFrom shiny div tagList NS HTML
-
+#'
 #' @param ... Widgets to be included in the panel.
 #' @param title An optional title for the panel. Default is NULL.
 #' @param bg_color Background color for the panel. Default is transparent.
@@ -17,6 +18,7 @@
 #' @param border_color Color of the panel border. Default is "lightgrey".
 #' @param border_type Type of the panel border. Default is "solid".
 #' @param margin_top Top margin for the panel. Default is "20px".
+#' @param height Height of the panel. Default is NULL (auto height).
 #' @return A shiny tag list containing the custom panel and its styles.
 #'
 #' @examples
@@ -34,7 +36,8 @@
 #'     border_thickness = "2px",
 #'     border_color = "blue",
 #'     border_type = "dashed",
-#'     margin_top = "15px"
+#'     margin_top = "15px",
+#'     height = "300px"  # Set the height here
 #' )
 #' }
 #' @export
@@ -49,7 +52,8 @@ titlePanel <- function(...,
                        border_thickness = "1px",
                        border_color = "lightgrey",
                        border_type = "solid",
-                       margin_top = "20px") {
+                       margin_top = "20px",
+                       height = NULL) {
 
   # If no ID provided, generate a unique ID using NS and timestamp
   if (is.null(id)) {
@@ -75,6 +79,13 @@ titlePanel <- function(...,
     "background-color: transparent;"
   }
 
+  # Height styling
+  height_css <- if (!is.null(height)) {
+    paste0("height: ", height, ";")
+  } else {
+    "height: auto;"
+  }
+
   # Create and return the custom panel with its styles
   shiny::tagList(
     shiny::tags$style(shiny::HTML(paste0(
@@ -85,6 +96,7 @@ titlePanel <- function(...,
             padding: 20px;
             position: relative;
             margin-top: ", margin_top, ";
+            ", height_css, "
         }
 
         #", id, " .custom-panel-header {
@@ -102,9 +114,8 @@ titlePanel <- function(...,
         }"
     ))),
     shiny::div(id = id, class = "custom-panel",
-        title_div,
-        widgets
+               title_div,
+               widgets
     )
   )
 }
-
